97d3ac4edbbd01354fca24bb63d166378815c3cb
Vladusick <samuelfs@mail.ru>
2023-04-01 15:47:46 +0400

    Initial commit

---
+++ src/main/java/SumServlet.java
@@ -0,0 +1,29 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SumServlet", urlPatterns = "/sum")
+public class SumServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(SumServlet.class);
+
+    // http://localhost:8080/java-ee/sum?a=1&b=1
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("text/html");
+
+        int firstNumber = Integer.parseInt(req.getParameter("a"));
+        int secondNumber = Integer.parseInt(req.getParameter("b"));
+        int sum = firstNumber + secondNumber;
+
+        out.println("<html><body><h1>" + String.format("%d + %d = %d",firstNumber, secondNumber, sum) + "</h1></body></html>");
+        out.close();
+    }
+}
